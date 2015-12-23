0a82b32ada2f146d961215f4481a16ba9f7d279b
nyrican466 <edwin.alvarado@hotmail.comgit config -1git config --global user.email edwin.alvarado@hotmail.com>
2015-12-23 17:19:21 -0500

    added greeting

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ require "sinatra/activerecord"
 
 set :database, "sqlite3:myblogdb.sqlite3"
 
+enable :sesions
+
 require "./models.rb"
+
+#*****************************
+
+	get '/' do
+		erb :index
+end
+
+get '/sign-in' do
+	erb :signin
+end
